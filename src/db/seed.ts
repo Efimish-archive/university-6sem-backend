78b0e201a17e243b8c3c6b5617f4396dec51fe43
Efim Ishenin <me@efima.ru>
2026-02-28 15:35:24 +0700

    add db seed script

---
+++ src/db/seed.ts
@@ -0,0 +1,87 @@
+import { faker, fakerRU } from "@faker-js/faker";
+import { db, schema } from ".";
+
+const createFakeRecipe = (cuisineId: number): typeof schema.recipes.$inferInsert => ({
+  title: fakerRU.food.dish(),
+  description: fakerRU.food.adjective(),
+  cookingTime: faker.number.int({ min: 1, max: 100 }),
+  difficulty: faker.number.int({ min: 1, max: 5 }),
+  cuisineId,
+});
+
+const createFakePost = (): typeof schema.posts.$inferInsert => ({
+  title: fakerRU.book.title(),
+  descr: fakerRU.book.genre(),
+});
+
+const createAmount = <T>(createFn: () => T, amount: number) => {
+  const result: T[] = [];
+  while (result.length < amount) {
+    result.push(createFn());
+  }
+  return result;
+}
+
+await db.delete(schema.recipeAllergens);
+await db.delete(schema.recipeIngredients);
+await db.delete(schema.recipes);
+await db.delete(schema.cuisines);
+await db.delete(schema.allergens);
+await db.delete(schema.ingredients);
+await db.delete(schema.posts);
+console.log("deleted the whole database");
+
+const cuisines = await db.insert(schema.cuisines).values(
+  faker.helpers.uniqueArray(
+    fakerRU.food.ethnicCategory, 100
+  ).map((name) => ({ name }))
+).returning();
+console.log("seeded cuisines");
+
+const allergens = await db.insert(schema.allergens).values(
+  faker.helpers.uniqueArray(
+    fakerRU.food.spice, 100
+  ).map((name) => ({ name }))
+).returning();
+console.log("seeded allergens");
+
+const ingredients = await db.insert(schema.ingredients).values(
+  faker.helpers.uniqueArray(
+    fakerRU.food.ingredient, 100
+  ).map((name) => ({ name }))
+).returning();
+console.log("seeded ingredients");
+
+const recipes = await db.insert(schema.recipes).values(
+  cuisines.flatMap((cuisine) =>
+    createAmount(() => createFakeRecipe(cuisine.id), 3)
+  )
+).returning();
+console.log("seeded recipes");
+
+await db.insert(schema.recipeAllergens).values(
+  faker.helpers.arrayElements(recipes)
+    .flatMap(({ id: recipeId }) => faker.helpers.arrayElements(allergens, {
+      min: 1,
+      max: 5,
+    }).map(({ id: allergenId }) => ({ recipeId, allergenId })))
+);
+console.log("seeded recipe allergens");
+
+await db.insert(schema.recipeIngredients).values(
+  recipes.flatMap(({ id: recipeId }) => faker.helpers.arrayElements(ingredients, {
+    min: 3,
+    max: 10,
+  }).map(({ id: ingredientId }) => ({
+    recipeId,
+    ingredientId,
+    quantity: faker.number.int({ min: 1, max: 20 }),
+    measurement: faker.helpers.enumValue(schema.MeasurementEnum),
+  })))
+);
+console.log("seeded recipe ingredients");
+
+await db.insert(schema.posts).values(
+  createAmount(createFakePost, 100)
+);
+console.log("seeded posts");
